879c7d1e47215be2eea42b6c573be3a29c9364d2
Chun Ho Andy Kwok <akwok@Andys-MacBook-Pro.local>
2018-02-08 00:10:29 +0800

    Complete the Phase 1 v1, need more testing and optimiation

--- Phase1.py
+++ Phase1.py
@@ -1,5 +1,22 @@
+from operator import itemgetter
+from datetime import datetime
+
 URL = None
+# offerings = {cid : [courseDetails]}
+# courseDetails = [CourseCode, Course Title, Credits, [Pre-re(Course Code)], [Exclusion(Course Code], Course Descr, [[sectionDetails](s)]]
+# sectionDetails = [Section Num, DateTime, Room, Instructor, Quota, Enrol, Avail, Wait, Remarks]
+courseOfferings = {'COMP4332L1':
+                       ['COMP4332', 'Big Data Mining and Management', 3,[], [], "This is a big data course that teaches problem solving",
+                        [['L1', '2018-02-01 15:30', 'G010', 'Prof Raymond', 100, 51, 49, 0,'can take' ]]],
+                   'RMBI4310L1':
+                       ['RMBI4310', 'Advanced Data Mining for Risk Management and Business Intelligence', 3,[], [], "This is a big data course that teaches problem solving",
+                        [['L1', '2018-02-01 15:30', 'G010', 'Prof Raymond', 100, 52, 48, 0,'can take' ]]],
+                   'COMP4333L1':
+                       ['COMP4333', 'Big Data Mining and Management', 3,[], [], "This is a big data course that teaches problem solving",
+                        [['L1', '2018-02-01 15:30', 'G010', 'Prof Raymond', 100, 51, 49, 0,'can take' ]]]
+                   }
 courses = ["COMP4332" , "ELEC1010", "COMP3221", "Big Data Management", "Dumb Data", "sth"]
+#courses = courseOfferings.keys()
 
 
 # 5.1
@@ -8,10 +25,10 @@ def dropAndEmptySuccessful():
 
 # 5.2
 def crawlData(enteredURL):
-    if (URL == "default"):
+    if enteredURL == "default":
         print("Data Crawling is successful and all data are inserted into the database")
     else:
-        url = enteredURL
+        URL = enteredURL
         print("Data Crawling is successful and all data are inserted into the database")
 
 # 5.3
@@ -21,31 +38,109 @@ def courseSearch():
     print("\t2. Course Search by Waiting List Size\n")
     choice = input("Please indicata the preferred operation\n")
 
-    matchedCourses = []
+
+
+    # keyword search
     if choice == "1":
         query = input("Please enter your keyword\n")
-        keywords = query.split()
-        for keyword in keywords:
-            for course in courses:
-                if (course.find(keyword) != -1 ):
-                    matchedCourses.append(course)
+        keywordSearch(query)
+    #waitlist search
     elif choice == "2":
-        #search
-        print("sth")
+        f = input("Please input f value")
+        start_ts = input("Please input start time (in a format of YYYY-MM-DD HH:mm)")
+        end_ts = input("Please input end time (in a format of YYYY-MM-DD HH:mm)")
+        waitingListSearch(f, start_ts, end_ts)
     else:
         print("Please enter a valid choice\n")
 
+def keywordSearch(query):
+    matchedCourses = []
+    keywords = query.split()
+    for keyword in keywords:
+        for course in courseOfferings.values():
+            courseKeywords = course[1].split()
+            for courseKeyword in courseKeywords:
+                if courseKeyword == keyword:
+                    matchedCourses.append(course)
+    matchedCourses = sortCoursesUsingCourseCode(matchedCourses)
     printAllCourses(matchedCourses)
 
+
+def sortCoursesUsingCourseCode(courses):
+    return sorted(courses, key = itemgetter(0))
+
+
 def printAllCourses(courses):
-    if courses == None:
+    if len(courses) == 0:
         print("There is no match")
     else:
-        print("Here is all the matched courses")
+        print("Here are all the matched courses")
         for course in courses:
-            print(course)
+            printACourse(course)
+
+def printACourse(courseDetails):
+    print('Course Code:', courseDetails[0])
+    print('Course Title', courseDetails[1])
+    print('No. of Units/Credits', courseDetails[2])
+    for section in courseDetails[6]:
+        print('\nSection Details: ')
+        print('\tSection', section[0])
+        print('\tDate & Time', section[1])
+        print('\tQuota', section[4])
+        print('\tEnrol', section[5])
+        print('\tAvail', section[6])
+        print('\tWait', section[7])
+        print('\n')
+    print('\n')
+
+# need testing
+def waitingListSearch(f, start_ts, end_ts):
+    f = float(f)
+    start_ts = datetime.strptime(start_ts, "%Y-%m-%d %H:%M")
+    end_ts = datetime.strptime(end_ts, "%Y-%m-%d %H:%M")
+
+    matchedCourseDetails = {}
+    for courseDetails in courseOfferings.values():
+        for sectionDetails in courseDetails[6]:
+            match_ts = datetime.strptime(sectionDetails[1], "%Y-%m-%d %H:%M")
+            if (sectionDetails[0][0] == 'L') and (start_ts <= match_ts <= end_ts):
+                copyedCourseDetails = courseDetails[:]
+                copyedCourseDetails.append(sectionDetails[1])
+
+                for matchedSectionDetails in copyedCourseDetails[6]:
+                    if float(matchedSectionDetails[7]) >= f * float(matchedSectionDetails[5]):
+                        matchedSectionDetails.append('Yes')
+                    else:
+                        matchedSectionDetails.append('No')
+
+                matchedCourseDetails[courseDetails[0]] = copyedCourseDetails
+    matchedCourseDetails = sorted(matchedCourseDetails.items(), key = itemgetter(0))
+    printAllMatched(matchedCourseDetails)
+
+def printAllMatched(matchedCourseDetails):
+    print(matchedCourseDetails)
+
+# 5.4
+def waitingListSizePrediction(cc, ln, ts):
+    print("n1, n2, n3, n4, n5")
+
+
+#5.5
+def  waitingListSizeTraining():
+    print("Waiting list size training is successful")
+
+
+
+
+
+
+
 
 def main():
     courseSearch()
+
+def test():
+    temp = datetime.strptime("2018-02-01 15:30", "%Y-%m-%d %H:%M")
+    print(temp)
 
 main()
